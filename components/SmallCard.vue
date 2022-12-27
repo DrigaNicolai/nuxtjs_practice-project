72fb2cd94f63ecc5e23ff3da166a27669d7ff551
DrigaNicolai <just.whyme7@gmail.com>
2022-12-27 20:40:54 +0200

    SmallCardDisplay component

---
+++ components/SmallCard.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: 'SmallCard',
+
+  props: {
+    card: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="card-container">
+    <img :src="require(`~/assets/images/${card.image || 'fe1.jpg'}`)" alt="small card" class="image">
+  </div>
+</template>
+
+<style scoped>
+.card-container {
+  width: 24%;
+  height: 12.5rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  margin-bottom: 1.5rem;
+  cursor: pointer;
+}
+.image {
+  width: 100%;
+  height: 100%
+}
+</style>
