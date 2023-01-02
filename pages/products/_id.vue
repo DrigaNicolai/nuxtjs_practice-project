75f9924790731d939f8ea5ebb5a34fb39a1d3df7
DrigaNicolai <just.whyme7@gmail.com>
2023-01-02 18:59:15 +0200

    Adding vue library with plugins

--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -18,6 +18,7 @@ export default {
       <div class="info-box">
         <h1>{{ product.title }}</h1>
         <p class="snippet">{{ product.snippet }}</p>
+        <RentModal />
       </div>
     </div>
     <div class="whats-included-container">
